19553ffd6f89b4cd5811028fffb6c229386000f0
cemalsenel <senelcemal61@gmail.com>
2021-07-29 12:39:53 +0200

    feat : update person permission

--- src/main/java/com/Security/config/PersonPermission.java
+++ src/main/java/com/Security/config/PersonPermission.java
@@ -3,8 +3,8 @@ package com.Security.config;
 
 public enum PersonPermission {
     USER_READ("user:read"),
-    ADMIN_WRITE("admin:read"),
-    ADMIN_READ("admin:write");
+    ADMIN_WRITE("admin:write"),
+    ADMIN_READ("admin:read");
 
     private final String permission;
 
