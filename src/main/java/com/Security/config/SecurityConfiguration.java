f7d6eb9d3508af4e2c711161821cbee776aa85f0
cemalsenel <senelcemal61@gmail.com>
2021-07-29 11:03:36 +0200

    feat : create Password Encoder

--- src/main/java/com/Security/config/SecurityConfiguration.java
+++ src/main/java/com/Security/config/SecurityConfiguration.java
@@ -1,25 +1,54 @@
 package com.Security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfiguration(PasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
+    }
+
 
     //WebSecurityConfigurerAdapter içerisindeki HTTP security metodunu override ediyoruz
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //http.authorizeRequests().anyRequest().permitAll();//şifreleri devre dışı bıraktık
         http.
+                csrf().//Cross-Site-Request-Forgery disabled
+                disable().
                 authorizeRequests().//istekleri denetle
-                anyRequest(). //tüm istekleri denetle
+                antMatchers("/","index","/css/*","/js/*"). //bu uzantılara ve anasayfaya şifresiz girişe izin ver
+                permitAll().
+                anyRequest(). //tüm istekleri denetlea
                 authenticated().//şifreli olarak kullan
                 and(). //farklı işlemleri birleştirebilmek için
                 formLogin(). //form login sayfası olarak giriş yapılsın
                 and().
                 httpBasic();//basic http kimlik denetimini kullan
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails user1 = User.builder().username("user").password(passwordEncoder.encode("12345")).roles("USER").build();
+
+        UserDetails admin1 = User.builder().username("admin").password(passwordEncoder.encode("123456")).roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user1, admin1);
     }
 }
